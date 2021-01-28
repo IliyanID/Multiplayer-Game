61a8a8581c90ea7a55c0e2a127d06859b2754adb
Iliyan Dimitrov <18351905+IliyanID@users.noreply.github.com>
2021-01-28 10:00:59 -0700

    Updated Pointer

---
+++ Front-End/src/Containers/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Dashboard from './DashBoard/Dashboard';
+import Login from './Login/Login';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+        <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Front-End/src/Containers/Login/Login.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Login.css';
+
+const Login: React.FC = () => {
+  return (
+    <div className="LoginMain">
+        <div>
+            <form>
+
+            </form>
+        </div>
+        
+    </div>
+  );
+}
+
+export default Login;
